42ea79cc857dbcd73805a36ad5218e807554aa2c
Jef <94884517+Ajeppp@users.noreply.github.com>
2022-03-15 13:25:40 +0700

    First

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World from DevTools!");
+
+
+window.setTimeout("waktu()", 1000);
+ 
+function waktu() {
+    var waktu = new Date();
+    setTimeout("waktu()", 1000);
+    document.getElementById("jam").innerHTML = waktu.getHours();
+    document.getElementById("menit").innerHTML = waktu.getMinutes();
+    document.getElementById("detik").innerHTML = waktu.getSeconds();
+}
