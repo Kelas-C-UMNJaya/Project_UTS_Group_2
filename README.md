fcbed7facd858a658d6949794c5b79e98d4e9d01
Steve Christian <94883724+stevgamz@users.noreply.github.com>
2022-03-18 14:32:20 +0700

    Perombakan JS dan Bagusin HTML&CSS

--- README.md
+++ README.md
@@ -16,25 +16,29 @@ Selamat datang di permainan Steve Life! sebelum bermain berikut adalah cara berm
 - Klik "Start" untuk mulai permainan.
 - Anda akan masuk ke menu game, di awal permainan semua progress bar akan terisi 50%, kecuali progress bar belajar yang dimulai dari 0%.
 - Permainan akan di mulai dari jam 11 siang.
+
 - Setiap anda klik "Eat" maka progress bar makan akan tambah.
-   > Ketika anda idle selama 12 menit digame, maka bar makan akan berkurang 2%.
-   > Jika button "Eat" di klik, setiap 1 menit digame bar makan akan bertambah 3%.
+   > - Ketika anda idle selama 12 menit digame, maka bar makan akan berkurang 2%.
+   > - Jika button "Eat" di klik, setiap 1 menit digame bar makan akan bertambah 3%.
+
 - Setiap anda klik "Study" maka progress bar belajar akan tambah, makan dan tidur akan berkurang.
-   > Kalau anda tidak belajar selama 4 jam digame maka anda akan di DO. (DIBERIKAN ALERT)
-   > Jika button "Study" di klik selama 15 menit digame, maka bar belajar akan bertambah 1%.
-   > Setiap anda belajar bar makan akan berkurang 1%, setiap 9 menit digame.
-   > Jika makan < 20% maka, growth dari belajar akan menjadi 30 menit digame = 1%. (DIBERIKAN ALERT)
-   > Setiap anda belajar bar tidur akan berkurang 1%, setiap 12 menit digame.
-   > Jika tidur < 20% maka, growth dari belajar akan menjadi 30 menit digame = 1%. (DIBERIKAN ALERT)
-   > Setiap anda belajar bar main akan berkurang 1%, setiap 10 menit digame.
+   > - Kalau anda tidak belajar selama 4 jam digame maka anda akan di DO. (DIBERIKAN ALERT)
+   > - Jika button "Study" di klik selama 15 menit digame, maka bar belajar akan bertambah 1%.
+   > - Setiap anda belajar bar makan akan berkurang 1%, setiap 9 menit digame.
+   > - Jika makan < 20% maka, growth dari belajar akan menjadi 30 menit digame = 1%. (DIBERIKAN ALERT)
+   > - Setiap anda belajar bar tidur akan berkurang 1%, setiap 12 menit digame.
+   > - Jika tidur < 20% maka, growth dari belajar akan menjadi 30 menit digame = 1%. (DIBERIKAN ALERT)
+   > - Setiap anda belajar bar main akan berkurang 1%, setiap 10 menit digame.
+
 - Setiap anda klik "Play" maka progress bar main akan bertambah, dan makan akan berkurang. 
-   > Jika anda idle selama 12 menit digame, maka bar main akan berkurang 1%.
-   > Jika button "Play" di klik selama 3 menit di game, bar main akan bertambah 1%.
-   > Setiap anda main bar makan akan berkurang 1%, setiap 10 menit digame.
-   > Jika bar main < 20%. (DIBERIKAN ALERT)
+   > - Jika anda idle selama 12 menit digame, maka bar main akan berkurang 1%.
+   > - Jika button "Play" di klik selama 3 menit di game, bar main akan bertambah 1%.
+   > - Setiap anda main bar makan akan berkurang 1%, setiap 10 menit digame.
+   > - Jika bar main < 20%. (DIBERIKAN ALERT)
+   
 - Setiap anda klik "Sleep" maka progress bar tidur akan tambah.
-   > Ketika anda idle selama 12 menit digame, maka bar tidur akan berkurang 1%
-   > Jika button "Sleep" diklik, selama 9 menit digame maka bar tidur akan bertambah 1%.
+   > - Ketika anda idle selama 12 menit digame, maka bar tidur akan berkurang 1%
+   > - Jika button "Sleep" diklik, selama 9 menit digame maka bar tidur akan bertambah 1%.
 
 
 
--- script.js
+++ script.js
@@ -2,115 +2,118 @@ let waktu = new Date();
 let jam = 11;
 let jamTemp = 0;
 let buttonType = "";
-
-// kurang / d n
-var kurangMakan = 0.4;
-var kurangTidur = 0.2;
-var kurangMain = 0.2;
-var kurangBelajar = 0;
-// tambah / d n
-var tambahMakan = 0;
-var tambahTidur = 0;
-var tambahMain = 0;
-var tambahBelajar = 0.4;
 var round = Math.round;
-// kumpulan variabel untuk digunakan pada function
+
+
+var increaseMakan = 0;
+var decreaseMakan = 0.6;
 let widthMakan = document.getElementById('bar-makan');
 let valueMakan = round(document.getElementById('bar-makan').ariaValueNow,0);
-let widthBelajar = document.getElementById('bar-belajar');
+var buttonmakan = document.querySelector("#button-makan");
+var klikMakan = true
+
+var increaseBelajar = 2;
+var decreaseBelajar = 0;
 let valueBelajar = round(document.getElementById('bar-belajar').ariaValueNow,0);
+let widthBelajar = document.getElementById('bar-belajar');
+var buttonbelajar = document.querySelector("#button-belajar");
+var klikBelajar = true
+
+
+var increaseMain = 0;
+var decreaseMain = 0.4;
 let widthMain = document.getElementById('bar-main');
 let valueMain = round(document.getElementById('bar-main').ariaValueNow,0);
+var buttonmain = document.querySelector("#button-main");
+var klikMain = true
+
+
+var increaseTidur = 0;
+var decreaseTidur = 0.5;
 let widthTidur = document.getElementById('bar-tidur');
 let valueTidur = round(document.getElementById('bar-tidur').ariaValueNow,0);
-
-var buttonmakan = document.querySelector("#button-makan");
-var buttonbelajar = document.querySelector("#button-belajar");
-var buttonmain = document.querySelector("#button-main");
 var buttontidur = document.querySelector("#button-tidur");
+var klikTidur = true
 
-var isClickedMakan = true
-var isClickedTidur = true
-var isClickedMain = true
-var isClickedBelajar = true
 
 buttonmakan.addEventListener("click", function() {
-    isClickedTidur = true;
-    isClickedMain = true;
-    isClickedBelajar = true;
-    if(isClickedMakan) {
-        buttonmakan.style.backgroundColor = "#EE9F16";
-        buttonbelajar.style.backgroundColor = "#FFFFFF";
-        buttontidur.style.backgroundColor = "#FFFFFF";
-        buttonmain.style.backgroundColor = "#FFFFFF";
-        buttonType= "bar-makan";
+  klikTidur = true;
+  klikMain = true;
+  klikBelajar = true;
+  if(klikMakan) {
+      buttonmakan.style.backgroundColor = "#EE9F16";
+      buttonbelajar.style.backgroundColor = "#E9D8A6";
+      buttontidur.style.backgroundColor = "#E9D8A6";
+      buttonmain.style.backgroundColor = "#E9D8A6";
+      buttonType= "bar-makan";
     }else{
-        buttonmakan.style.backgroundColor = "#FFFFFF";
-        buttonType= "";
+      buttonmakan.style.backgroundColor = "#E9D8A6";
+      buttonType= "";
     }
-    isClickedMakan = !isClickedMakan;
-});
-
-buttontidur.addEventListener("click", function() {
-    isClickedMakan = true;
-    isClickedMain = true;
-    isClickedBelajar = true;
-    if(isClickedTidur) {
-        buttonmakan.style.backgroundColor = "#FFFFFF";
-        buttonbelajar.style.backgroundColor = "#FFFFFF";
-        buttontidur.style.backgroundColor = "#EE9F16";
-        buttonmain.style.backgroundColor = "#FFFFFF";
-        buttonType= "bar-tidur";
-    }else{
-        buttontidur.style.backgroundColor = "#FFFFFF";
-        buttonType= "";
-    }
-    isClickedTidur = !isClickedTidur;
-    });
-
-buttonmain.addEventListener("click", function() {
-        isClickedTidur = true;
-        isClickedBelajar = true;
-        isClickedMakan = true;
-        if(isClickedMain) {
-            buttonmakan.style.backgroundColor = "#FFFFFF";
-            buttonbelajar.style.backgroundColor = "#FFFFFF";
-            buttontidur.style.backgroundColor = "#FFFFFF";
-            buttonmain.style.backgroundColor = "#EE9F16";
-            buttonType= "bar-main";
-        }else{
-            buttonmain.style.backgroundColor = "#FFFFFF";
-            buttonType= "";
-        }
-        isClickedMain = !isClickedMain;
-    });
-    
-buttonbelajar.addEventListener("click", function() {
-        isClickedTidur = true;
-        isClickedMain = true;
-        isClickedMakan = true;
-        if(isClickedBelajar) {
-            buttonmakan.style.backgroundColor = "#FFFFFF";
+    klikMakan = !klikMakan;
+  });
+  
+  buttonbelajar.addEventListener("click", function() {
+        klikTidur = true;
+        klikMain = true;
+        klikMakan = true;
+        if(klikBelajar) {
+            buttonmakan.style.backgroundColor = "#E9D8A6";
             buttonbelajar.style.backgroundColor = "#EE9F16";
-            buttontidur.style.backgroundColor = "#FFFFFF";
-            buttonmain.style.backgroundColor = "#FFFFFF";
+            buttontidur.style.backgroundColor = "#E9D8A6";
+            buttonmain.style.backgroundColor = "#E9D8A6";
             buttonType= "bar-belajar";
         }else{
-            buttonbelajar.style.backgroundColor = "#FFFFFF";
+            buttonbelajar.style.backgroundColor = "#E9D8A6";
             buttonType= "";
         }
-        isClickedBelajar = !isClickedBelajar;
-});
+        klikBelajar = !klikBelajar;
+  });
+  
+  buttonmain.addEventListener("click", function() {
+    klikTidur = true;
+    klikBelajar = true;
+    klikMakan = true;
+    if(klikMain) {
+          buttonmakan.style.backgroundColor = "#E9D8A6";
+          buttonbelajar.style.backgroundColor = "#E9D8A6";
+          buttontidur.style.backgroundColor = "#E9D8A6";
+          buttonmain.style.backgroundColor = "#EE9F16";
+          buttonType= "bar-main";
+        }else{
+          buttonmain.style.backgroundColor = "#E9D8A6";
+          buttonType= "";
+        }
+        klikMain = !klikMain;
+      });
 
-function nameValue() {
+      buttontidur.addEventListener("click", function() {
+        klikMakan = true;
+        klikMain = true;
+        klikBelajar = true;
+        if(klikTidur) {
+          buttonmakan.style.backgroundColor = "#E9D8A6";
+          buttonbelajar.style.backgroundColor = "#E9D8A6";
+          buttontidur.style.backgroundColor = "#EE9F16";
+          buttonmain.style.backgroundColor = "#E9D8A6";
+          buttonType= "bar-tidur";
+        }else{
+          buttontidur.style.backgroundColor = "#E9D8A6";
+          buttonType= "";
+        }
+        klikTidur = !klikTidur;
+      });
+      
+      function nameValue() {
   var name=document.getElementById("nama").value;
   localStorage.setItem("textvalue",name);
   return false; 
 }
   document.getElementById("result").innerHTML=localStorage.getItem("textvalue");
 
+
 const Time = setInterval(gameTime,1000);
-function gameTime(){
+function gameTime() {
     let salam = "";
     let waktu = new Date();
     let detik = waktu.getSeconds();
@@ -127,68 +130,62 @@ function gameTime(){
     }
     document.getElementById("waktu").innerHTML = jam + ":" + detik;
 
-    if (jam >= 6 && jam <= 11){
+    if (jam >= 6 && jam <= 11) {
         salam = "Good Morning";            
-    }else if (jam >= 12 && jam <= 15){
+    }else if (jam >= 12 && jam <= 15) {
         salam = "Good Afternoon"; 
-    }else if (jam >= 16 && jam <= 18){
+    }else if (jam >= 16 && jam <= 18) {
         salam = "Good Evening";
-    }else if (jam >= 19 && jam <= 24){
+    }else if (jam >= 19 && jam <= 24) {
         salam = "Good Night";
-    }else if (jam >= 1 && jam <= 5){
+    }else if (jam >= 1 && jam <= 5) {
         salam = "Good Night";
     }
     document.getElementById("mengucapSalam").innerHTML = salam;
    
     if (buttonType == "") {
-      // redeclare variable
-      kurangMakan = 0.4;
-      kurangTidur = 0.2;
-      kurangMain = 0.2;
+      decreaseMakan = 0.8;
+      decreaseTidur = 0.2;
+      decreaseMain = 0.4;
 
       if (valueMakan >= 20 ) {
-          kurangMain = 0.2;
+          decreaseMain = 0.4;
       }
       else if (valueMakan < 20) {
-          kurangMain = 0.4;
+          decreaseMain = 0.6;
       }
       if (valueMakan <= 0) {
           valueMakan = 0;
           widthMakan.style.width = valueMakan+"%"; 
       }else{
-          valueMakan = valueMakan - kurangMakan;
+          valueMakan = valueMakan - decreaseMakan;
           widthMakan.style.width = valueMakan+"%";    
       }
       if (valueTidur <= 0) {
           valueTidur = 0;
           widthTidur.style.width = valueTidur+"%";
       }else{
-          valueTidur = valueTidur - kurangTidur;
+          valueTidur = valueTidur - decreaseTidur;
           widthTidur.style.width = valueTidur+"%";
       }
       if (valueMain <= 0) {
           valueMain = 0;
           widthMain.style.width = valueMain+"%";
       }else{
-          valueMain = valueMain - kurangMain;
+          valueMain = valueMain - decreaseMain;
           widthMain.style.width = valueMain+"%";
       }
 
-  }else if (buttonType == "bar-makan") {
-      // finish
-      kurangMakan = 0.4;
-      kurangTidur = 0.2;
-      kurangMain = 0.2;
-
-      tambahMain = 3;
-      tambahMakan = 10;
-      valueMakan = valueMakan + tambahMakan;
+    }else if (buttonType == "bar-makan") {
+      decreaseMakan = 0.6;
+      decreaseTidur = 0.1;
+      decreaseMain = 0.4;
+      increaseMakan = 9;
+      
+      valueMakan = valueMakan + increaseMakan;
       widthMakan.style.width = valueMakan+"%";
 
-      valueMain = valueMain + tambahMain;
-      widthMain.style.width = valueMain+"%";
-
-      valueTidur = valueTidur - kurangTidur;
+      valueTidur = valueTidur - decreaseTidur;
       widthTidur.style.width = valueTidur+"%";
       console.log(valueMakan);
       if (valueMakan > 99) {
@@ -197,113 +194,114 @@ function gameTime(){
 
           valueTidur = valueTidur - 2;
           widthTidur.style.width = valueTidur + "%";
+          avatarTemp = avatar;
           buttonType = "";
       }
-  }else if (buttonType == "bar-tidur") {
-      kurangMakan = 0.4;
-      kurangTidur = 0.1;
-      kurangMain = 0.2;
-      tambahTidur = 1;
+    }else if (buttonType == "bar-belajar") {
+        decreaseMakan = 0.6;
+        decreaseTidur = 0.2;
+        decreaseMain = 0.3;
+
+        if (valueMain > 90) {
+            increaseBelajar = 2;
+        }
+        else if(valueMain < 20){
+            increaseBelajar = 0.2;
+        }
+        else if (valueMain < 10) {
+            increaseBelajar = 0.1;
+        }
+        else if (valueMakan < 20) {
+            increaseBelajar = 0.1;
+        }
+        else if (valueTidur < 20) {
+            increaseBelajar = 0.1;
+        }
+        else{
+            increaseBelajar = 1.5;
+        }
+        
+        if (valueMakan <= 0) {
+            valueMakan = 0;
+            widthMakan.style.width = valueMakan+"%"; 
+        }else{
+            valueMakan = valueMakan - decreaseMakan;
+            widthMakan.style.width = valueMakan+"%";    
+        }
+        if (valueTidur <= 0) {
+            valueTidur = 0;
+            widthTidur.style.width = valueTidur+"%";
+        }else{
+            valueTidur = valueTidur - decreaseTidur;
+            widthTidur.style.width = valueTidur+"%";
+        }
+        if (valueMain <= 0) {
+            valueMain = 0;
+            widthMain.style.width = valueMain+"%";
+        }else{
+            valueMain = valueMain - decreaseMain;
+            widthMain.style.width = valueMain+"%";
+        }
+        
+        hoursTemp = 0;
+        valueBelajar = valueBelajar + increaseBelajar;
+        widthBelajar.style.width = valueBelajar+"%";
+
+    }else if (buttonType == "bar-main") {
+      decreaseMakan = 0.6;
+      decreaseTidur = 0.2;
+      decreaseMain = 0.4;
+      increaseMain = 2;
 
       if (valueMakan <= 0) {
           valueMakan = 0;
           widthMakan.style.width = valueMakan+"%"; 
       }else{
-          valueMakan = valueMakan - kurangMakan;
-          widthMakan.style.width = valueMakan+"%";    
-      }
-      if (valueMain <= 0) {
-          valueMain = 0;
-          widthMain.style.width = valueMain+"%";
-      }else{
-          valueMain = valueMain - kurangMain;
-          widthMain.style.width = valueMain+"%";
-      }
-
-      valueTidur = valueTidur + tambahTidur;
-      widthTidur.style.width = valueTidur+"%";
-
-      if (valueTidur > 99) {
-          buttonType = "";
-      }
-
-  }else if (buttonType == "bar-main") {
-      kurangMakan = 0.4;
-      kurangTidur = 0.2;
-      kurangMain = 0.2;
-      tambahMain = 0.8;
-
-      if (valueMakan <= 0) {
-          valueMakan = 0;
-          widthMakan.style.width = valueMakan+"%"; 
-      }else{
-          valueMakan = valueMakan - kurangMakan;
+          valueMakan = valueMakan - decreaseMakan;
           widthMakan.style.width = valueMakan+"%";    
       }
       if (valueTidur <= 0) {
           valueTidur = 0;
           widthTidur.style.width = valueTidur+"%";
       }else{
-          valueTidur = valueTidur - kurangTidur;
+          valueTidur = valueTidur - decreaseTidur;
           widthTidur.style.width = valueTidur+"%";
       }
       
-      valueMain = valueMain + tambahMain;
+      valueMain = valueMain + increaseMain;
       widthMain.style.width = valueMain+"%"; 
 
       if (valueMain > 99) {
+          avatarTemp = avatar;
           buttonType = "";
       }
-  }else if (buttonType == "bar-belajar") {
-      // finish
-      kurangMakan = 0.6;
-      kurangTidur = 0.2;
-      kurangMain = 0.3;
+    }else if (buttonType == "bar-tidur") {
+      decreaseMakan = 0.6;
+      decreaseTidur = 0.2;
+      decreaseMain = 0.4;
+      increaseTidur = 2;
 
-      if (valueMain > 90) {
-          tambahBelajar = 0.5;
-      }
-      else if(valueMain < 20){
-          tambahBelajar = 0.2;
-      }
-      else if (valueMain < 10) {
-          tambahBelajar = 0.1;
-      }
-      else if (valueMakan < 20) {
-          tambahBelajar = 0.1;
-      }
-      else if (valueTidur < 20) {
-          tambahBelajar = 0.1;
-      }
-      else{
-          tambahBelajar = 0.4;
-      }
-      // klau sudah 0 akan stop
       if (valueMakan <= 0) {
           valueMakan = 0;
           widthMakan.style.width = valueMakan+"%"; 
       }else{
-          valueMakan = valueMakan - kurangMakan;
+          valueMakan = valueMakan - decreaseMakan;
           widthMakan.style.width = valueMakan+"%";    
-      }
-      if (valueTidur <= 0) {
-          valueTidur = 0;
-          widthTidur.style.width = valueTidur+"%";
-      }else{
-          valueTidur = valueTidur - kurangTidur;
-          widthTidur.style.width = valueTidur+"%";
       }
       if (valueMain <= 0) {
           valueMain = 0;
           widthMain.style.width = valueMain+"%";
       }else{
-          valueMain = valueMain - kurangMain;
+          valueMain = valueMain - decreaseMain;
           widthMain.style.width = valueMain+"%";
       }
-      
-      hoursTemp = 0;
-      valueBelajar = valueBelajar + tambahBelajar;
-      widthBelajar.style.width = valueBelajar+"%";
-  }
-}
 
+      valueTidur = valueTidur + increaseTidur;
+      widthTidur.style.width = valueTidur+"%";
+
+      if (valueTidur > 99) {
+          avatarTemp = avatar;
+          buttonType = "";
+      }
+    }
+}
